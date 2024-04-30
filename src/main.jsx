88fd8c41dfe63a6e6875f30dc33216e4c842d87d
yeeun <84173008+jye104@users.noreply.github.com>
2024-04-30 18:21:55 +0900

    feat: 탄스텍 쿼리, 데브툴 설치 및 쿼리 클라이언트 셋팅

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,24 @@ import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { Provider } from 'react-redux';
 import store from './store';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: 0,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </Provider>
 );
